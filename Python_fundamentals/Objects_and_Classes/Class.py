459d3e8f10a3760cd36add1845aac0189336f931
vbukovska <vbukovska@yahoo.com>
2020-10-28 23:51:46 +0200

    Adding exercises - Objects and Classes

---
+++ Python_fundamentals/Objects_and_Classes/Class.py
@@ -0,0 +1,30 @@
+class Class:
+    __students_count = 22
+
+    def __init__(self, name):
+        self.name = name
+        self.students = []
+        self.grades = []
+
+    def add_student(self, name, grade):
+        if len(self.students) < self.__students_count:
+            self.students.append(name)
+            self.grades.append(grade)
+
+    def get_average_grade(self):
+        average_grade = sum(self.grades) / len(self.grades)
+        return average_grade
+
+    def __repr__(self):
+        class_name = self.name
+        students_in_class = ', '.join(self.students)
+        class_avg_grade = self.get_average_grade()
+        return f"The students in {class_name}: {students_in_class}. Average grade: {class_avg_grade:.2f}"
+
+
+a_class = Class("11B")
+a_class.add_student("Peter", 4.80)
+a_class.add_student("George", 6.00)
+a_class.add_student("Amy", 3.50)
+print(a_class)
+
